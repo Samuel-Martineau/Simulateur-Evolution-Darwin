c29e5cc5f75f0c4b8281e29aeaaa89f0e29e6ee7
Samuel-Martineau <samumartineau@gmail.com>
2019-09-25 20:41:02 -0400

    Correction d'une erreur dans la config de webpack

--- webpack/webpack.prod.config.js
+++ webpack/webpack.prod.config.js
@@ -1,9 +1,8 @@
 const merge = require('webpack-merge');
 const webpackBaseConfig = require('./webpack.common.config.js');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 module.exports = merge(webpackBaseConfig, {
   optimization: {
-    minimizer: [new UglifyJsPlugin(), new OptimizeCSSAssetsPlugin()]
+    minimizer: [new OptimizeCSSAssetsPlugin()]
   }
 });
