23e1a81b8fd447c755108293aa9055f385b22c3b
Samuel-Martineau <samumartineau@gmail.com>
2021-01-22 13:21:53 -0500

    Correction du titre de la page d'accueil #20

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -10,14 +10,16 @@
   import { description } from "../../package.json";
 </script>
 
-<svelte:head>Simuler l'évolution</svelte:head>
+<svelte:head>
+  <title>Simuler l'évolution</title>
+</svelte:head>
 
 <Cover>
   <span slot="title">Simuler l'évolution</span>
   <span slot="description">{description}</span>
   <span slot="actions">
-    <Button color="green" style="font-size: 25px" href="/demarrer"
-      >Démarrer</Button
-    >
+    <Button color="green" style="font-size: 25px" href="/demarrer">
+      Démarrer
+    </Button>
   </span>
 </Cover>
